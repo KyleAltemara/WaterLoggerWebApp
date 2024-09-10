98aa053d4c6c0a4f0977b40897480a13b7fdc77e
Kyle <Kyle.Altemara@gmail.com>
2024-09-10 12:49:08 -0700

    Added data annotations to the WaterRecordModel for validation and formatting

--- WaterLoggerWebApp/Models/WaterRecordModel.cs
+++ WaterLoggerWebApp/Models/WaterRecordModel.cs
@@ -1,8 +1,14 @@
-﻿namespace WaterLoggerWebApp.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WaterLoggerWebApp.Models;
 
 public class WaterRecordModel
 {
     public int Id { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:dd-MMM-yy}", ApplyFormatInEditMode = true)]
     public DateTime Date { get; set; }
+    
+    [Range(0, int.MaxValue, ErrorMessage = "Value for {0} must be positive.")]
     public int Quantity { get; set; }
 }
