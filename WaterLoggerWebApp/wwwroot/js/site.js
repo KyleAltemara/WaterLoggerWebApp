aa75dda0542bb691082b9110ae6051be8d5ea300
Kyle <Kyle.Altemara@gmail.com>
2024-09-10 12:48:19 -0700

    Added button and JavaScript to to calculate the total water drunk

--- WaterLoggerWebApp/wwwroot/js/site.js
+++ WaterLoggerWebApp/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function calculate() {
+    var records = document.getElementById("records");
+    var total = 0;
+    for (var i = 1; i < records.rows.length; i++) {
+        var row = records.rows[i];
+        var cells = row.cells;
+        var cell = cells[1];
+        var value = parseInt(cell.innerHTML);
+        total += value;
+    }
+
+    var result = document.getElementById("result");
+    result.append(`Total: ${total}`)
+}
